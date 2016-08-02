95b54e307111d169479ec3b90cd0f0313c5723ed
Arnaud <diegoarno@gmail.com>
2016-08-03 00:24:00 +0200

    full commit

---
+++ toggle_touchscreen.ps1
@@ -0,0 +1,10 @@
+﻿import-Module ./DeviceManagement.psd1
+
+$Device = 'Intel(R) Precise Touch Device'
+$Status = Get-Device | Where-Object -Property Name -eq $Device | foreach {$_.ConfigurationFlags}
+If ($Status -eq ‘CONFIGFLAG_DISABLED’) {
+$deviceName = 'Intel(R) Precise Touch Device'; Get-Device | Where-Object -Property Name -eq $deviceName | Enable-Device -Confirm:$false
+}
+Else { 
+$deviceName = 'Intel(R) Precise Touch Device'; Get-Device | Where-Object -Property Name -eq $deviceName | Disable-Device -Confirm:$false
+}
